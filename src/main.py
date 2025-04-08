a2f7084f8b63f68fcdc62ad5e78d51474ef26ddc
Ashton <38774365+ashton-southall@users.noreply.github.com>
2025-04-08 12:30:04 +1000

    Update main.py
    Code Cleanup

--- src/main.py
+++ src/main.py
@@ -3,30 +3,34 @@ import network
 import socket
 import time
 import machine
-import sys
-import secrets
+import secrets # Ensure a file named secrets.py is present in file structure, containing SSID details
 
-# Initialize an external LED on GPIO 15
+# Initialize an external LED on GPIO 16
 led = machine.Pin(16, machine.Pin.OUT)  # Set GPIO 16 as an output
 
-def connect():
-    #Connect to WLAN
+def connect(): # Connect to WLAN - SSID in secrets.py
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     pico_led.blink(0.1)
     wlan.connect(secrets.ssid)
+    
     if wlan.isconnected() == False:
         print('Waiting for connection...')
         time.sleep(1)
+        
     if wlan.isconnected() == True:
         print("ARNI-SCOUT Connected")
+        
 def check():
     try:
         pico_led.on()
         led.on()
         wlan = network.WLAN(network.STA_IF)
+        
         if wlan.isconnected() == False:
             connect()
+            return
+        
         addr = socket.getaddrinfo("google.com", 80)[0][-1]  # Resolve Google’s IP
         s = socket.socket()
         s.connect(addr)
@@ -37,14 +41,16 @@ def check():
         pico_led.off()
         led.off()
         s.close()
+        
     except Exception as e:
         print("Ping failed:", e)
         led.on()
 
-pico_led.blink(0.1)
 led.off()
+pico_led.blink(0.1)
 print("Booting")
 connect()
+
 while True:
     check()
     time.sleep(5)
